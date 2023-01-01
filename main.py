3fd3274ddfe02fb16f814e2867fe127675955b60
Robert Dyson <rdyson101@gmail.com>
2023-01-01 12:44:01 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+#Import random module to access random.choice
 import random
 
-
+#Get the choice of the player via input and the computer via random.choice
 def get_choices():
     player_choice = input("Enter a choice (rock, paper, scissors): ")
     options = ["rock", "paper", "scissors"]
@@ -8,6 +9,7 @@ def get_choices():
     choices = {"player": player_choice, "computer": computer_choice}
     return choices
 
+#check win conditions with nested functions
 def check_win(player, computer):
     print(f"You chose {player}, computer chose {computer}.")
     if player == computer:
@@ -31,18 +33,20 @@ def check_win(player, computer):
         if player != "rock" or "scissors" or "paper":
             return "You idiot, that's not a good choice, YOU ARE A LOSER!"
 
+#Define the functions of the game        
 def game():
     choices = get_choices()
     result = check_win(choices["player"], choices["computer"])
     print(result)
     restart()
 
+#Restart the game function
 def restart():
     restart = input("Do you wish to play again?(y or n): ")
     if restart == "y":
-        os.system("cls")
         game()
     else:
         quit()
-game()
 
+#run the game        
+game()
